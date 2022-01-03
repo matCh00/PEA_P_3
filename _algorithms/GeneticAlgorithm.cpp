c7df2e151780ea3835a1b9c03fe1ee7667ed62db
matCh00 <mateusz.chalik.g@gmail.com>
2022-01-03 23:42:55 +0100

    krzyżowanie i jej metody

--- _algorithms/GeneticAlgorithm.cpp
+++ _algorithms/GeneticAlgorithm.cpp
@@ -218,3 +218,182 @@ vector<int> GeneticAlgorithm::tournamentSelection(vector<vector<int>> pop) {
 
     return best;
 }
+
+
+
+void GeneticAlgorithm::doCrossover(vector<int> parent1, vector<int> parent2, vector<int> &offspring1, vector<int> &offspring2) {
+
+    switch (crossType) {
+        case 2:
+            OrderCO(parent1, parent2, offspring1, offspring2); // jest gituwa
+            break;
+
+            case 7:
+                EnhancedSequentialCO(parent1, parent2, offspring1); //jest gituwa
+                break;
+    }
+}
+
+
+
+void GeneticAlgorithm::OrderCO(vector<int> parent1, vector<int> parent2, vector<int> &offspring1, vector<int> &offspring2) {
+
+    random_device randomSrc;
+    default_random_engine randomGen(randomSrc());
+    uniform_int_distribution<> nodeRand(1, matrixSize - 1);
+
+    vector<int> visitedOffspring1(matrixSize, 0);
+    vector<int> visitedOffspring2(matrixSize, 0);
+
+
+    int a, b;
+
+    do {
+        a = nodeRand(randomGen);
+        b = nodeRand(randomGen);
+    } while (a == b || a > b);
+
+
+    for (int i = a; i < b; i++) {
+        offspring1.at(i) = parent1.at(i);
+        offspring2.at(i) = parent2.at(i);
+        visitedOffspring1.at(parent1.at(i)) = 1;
+        visitedOffspring2.at(parent2.at(i)) = 1;
+    }
+
+
+    int omitted = 0, omitted2 = 0;
+
+    for (int i = b; i < matrixSize; i++) {
+        if (visitedOffspring1.at(parent2.at(i)) != 1) {
+            offspring1.at(i - omitted) = parent2.at(i);
+            visitedOffspring1.at(parent2.at(i)) = 1;
+        } else
+            omitted++;
+
+        if (visitedOffspring2.at(parent1.at(i)) != 1) {
+            offspring2.at(i - omitted2) = parent1.at(i);
+            visitedOffspring2.at(parent1.at(i)) = 1;
+        } else
+            omitted2++;
+
+    }
+
+    int helpOmitted1 = omitted, helpOmitted2 = omitted2;
+
+    for (int i = 1; i < b; i++) {
+
+        if (visitedOffspring1.at(parent2.at(i)) != 1) {
+            if (omitted != 0) {
+                offspring1.at(matrixSize - omitted) = parent2.at(i);
+                visitedOffspring1.at(parent2.at(i)) = 1;
+                omitted--;
+            } else {
+                offspring1.at(i - helpOmitted1) = parent2.at(i);
+                visitedOffspring1.at(parent2.at(i)) = 1;
+            }
+        } else {
+            helpOmitted1++;
+        }
+
+
+        if (visitedOffspring2.at(parent1.at(i)) != 1) {
+            if (omitted2 != 0) {
+                offspring2.at(matrixSize - omitted2) = parent1.at(i);
+                visitedOffspring2.at(parent1.at(i)) = 1;
+                omitted2--;
+            } else {
+                offspring2.at(i - helpOmitted2) = parent1.at(i);
+                visitedOffspring2.at(parent1.at(i)) = 1;
+            }
+        } else {
+            helpOmitted2++;
+        }
+
+    }
+
+    offspring1.at(matrixSize + 1) = calculateCost(offspring1);
+    offspring2.at(matrixSize + 1) = calculateCost(offspring2);
+
+}
+
+
+
+void GeneticAlgorithm::EnhancedSequentialCO(vector<int> parent1, vector<int> parent2, vector<int> &offspring) {
+
+    int omitted = 0, inserted = 0;
+    vector<int> visitedOffspring(matrixSize, 0);
+    vector<int> indexParent1(matrixSize, 0);
+    vector<int> indexParent2(matrixSize, 0);
+
+    for (int i = 0; i < matrixSize; i++) {
+        indexParent1.at(parent1.at(i)) = i;
+        indexParent2.at(parent2.at(i)) = i;
+    }
+
+    offspring.at(0) = 0;
+
+    int node1, node2, minimum1 = INT_MAX, minimum2 = INT_MAX;
+
+    for (int i = 0; i < matrixSize; i++) {
+        if (visitedOffspring.at(i) != 1 && matrix[parent1.at(1)][i] < minimum1)
+            minimum1 = matrix[parent1.at(1)][i];
+
+        if (visitedOffspring.at(i) != 1 && matrix[parent2.at(1)][i] < minimum2)
+            minimum2 = matrix[parent2.at(1)][i];
+    }
+
+    if (matrix[0][parent1.at(1)] + minimum1 < matrix[0][parent2.at(1)] + minimum2) {
+        offspring.at(1) = parent1.at(1);
+        visitedOffspring.at(parent1.at(1)) = 1;
+    } else {
+        offspring.at(1) = parent2.at(1);
+        visitedOffspring.at(parent2.at(1)) = 1;
+    }
+
+    for (int i = 2; i < matrixSize; i++) {
+        if (indexParent1.at(offspring.at(i - 1)) + 1 <= matrixSize - 1 &&
+        visitedOffspring.at(parent1.at(indexParent1.at(offspring.at(i - 1)) + 1)) != 1) {
+            node1 = parent1.at(indexParent1.at(offspring.at(i - 1)) + 1);
+        } else {
+            for (int j = 1; j < matrixSize; j++)
+                if (visitedOffspring.at(j) != 1) {
+                    node1 = j;
+                    break;
+                }
+        }
+        if (indexParent2.at(offspring.at(i - 1)) + 1 <= matrixSize - 1 &&
+        visitedOffspring.at(parent2.at(indexParent2.at(offspring.at(i - 1)) + 1)) != 1) {
+            node2 = parent2.at(indexParent2.at(offspring.at(i - 1)) + 1);
+        } else {
+            for (int j = 1; j < matrixSize; j++)
+                if (visitedOffspring.at(j) != 1) {
+                    node2 = j;
+                    break;
+                }
+        }
+
+        minimum1 = INT_MAX, minimum2 = INT_MAX;
+
+        for (int j = 0; j < matrixSize; j++) {
+            if (visitedOffspring.at(j) != 1 && matrix[parent1.at(1)][j] < minimum1)
+                minimum1 = matrix[node1][j];
+
+            if (visitedOffspring.at(j) != 1 && matrix[parent2.at(1)][j] < minimum2)
+                minimum2 = matrix[node2][j];
+        }
+
+        if (matrix[offspring.at(i - 1)][node1] + minimum1 < matrix[offspring.at(i - 1)][node2] + minimum2) {
+            offspring.at(i) = node1;
+            visitedOffspring.at(node1) = 1;
+        } else {
+            offspring.at(i) = node2;
+            visitedOffspring.at(node2) = 1;
+        }
+    }
+
+    offspring.at(matrixSize + 1) = calculateCost(offspring);
+
+    //showVector(offspring);
+}
+
